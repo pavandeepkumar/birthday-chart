0c26f29208128597b0076878cde9b44c86a7cddf
pavandeep.devstree <pavandeep.devstree@gmail.com>
2024-04-07 20:12:53 +0530

    done birthday chart

--- src/components/HomeComponets/HomeComponents.jsx
+++ src/components/HomeComponets/HomeComponents.jsx
@@ -6,13 +6,10 @@ const HomeComponents = () => {
             <div className='bg-[#e8dee4] w-full h-full border-[3px] rounded-2xl border-[#c3bcbe]'>
                 <h2 className='text-center text-2xl text-[#533272] font-semibold mx-6 py-3 border-b-[3px] border-[#c3bcbe]'>Birth Chart</h2>
                 <div className='flex justify-center items-center h-full'>
-                <div className='w-[439px] absolute left-[580px] bg-white flex rotate-45  border-[1px] border-[#a39a9d]'></div>
-                <div className='w-[439px] absolute left-[580px] bg-white flex -rotate-45  border-[1px] border-[#a39a9d]'></div>
-                    <div className='w-80 bg-white flex items-center justify-center h-80  border-[2px] border-[#a39a9d]'>
-                
-                    
+                    <div className='w-[439px] absolute left-[580px] bg-white flex rotate-45  border-[1px] border-[#a39a9d]'></div>
+                    <div className='w-[439px] absolute left-[580px] bg-white flex -rotate-45  border-[1px] border-[#a39a9d]'></div>
+                    <div className='w-80 bg-[#eaeaea] flex items-center justify-center h-80  border-[2px] border-[#a39a9d]'>
                         <div className='w-[225px]  h-[225px]  border-[2px] rotate-45 border-[#a39a9d]'>
-
                         </div>
                     </div>
                 </div>
